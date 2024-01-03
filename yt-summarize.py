2c6d48f550788433c93a91741e1f2c463eb6b0cd
FlareXes <70066527+FlareXes@users.noreply.github.com>
2024-01-03 06:59:50 +0530

    Store Prompts Externally - Done

--- yt-summarize.py
+++ yt-summarize.py
@@ -5,10 +5,29 @@ import yt_dlp
 import json
 import sys
 
+
+def get_prompt(prompt_name="detailed"):
+    with open("./prompts.json", "r") as f:
+        prompt_data = json.load(f)
+        prompts = prompt_data.get("prompts")
+
+        for prompt in prompts:
+            if prompt["name"] == prompt_name:
+                return prompt.get("content")
+
+
+def generate_prompt():
+    with open("/tmp/subtitle.en.vtt", "r") as f:
+        subtitle = f.read()
+    prompt = get_prompt()
+    sep = "\n\n-----------------------\n\n"
+    return subtitle + sep + prompt
+
+
 def download_subtitle(url):
     ydl_opts = {
         "skip_download": True,
-        "outtmpl": {"default": "subtitle"},
+        "outtmpl": {"default": "/tmp/subtitle"},
         "writesubtitles": True,
         "writeautomaticsub": True,
         "subtitleslangs": ["en"],
@@ -17,45 +36,35 @@ def download_subtitle(url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.extract_info(url)
 
-def get_prompt():
-    with open("./subtitle.en.vtt", "r") as f:
-        subtitle = f.read()
-
-    prompt = """
----------
-
-Can you provide a comprehensive summary of the given transcribe? The summary should cover all the key points and main ideas presented in the original text, while also condensing the information into a concise and easy-to-understand format. Please ensure that the summary includes relevant details and examples that support the main ideas, while avoiding any unnecessary information or repetition. The length of the summary should be appropriate for the length and complexity of the original text, providing a clear and accurate overview without omitting any important information.
-And, Can you determine from the provided transcription whether the video is a practical demonstration or not?
-"""
-    return subtitle + prompt
 
 def summarize(prompt):
     # API endpoint
     api_url = "http://localhost:11434/api/generate"
 
     # Request data
-    data = {"model": "llama2-uncensored", "prompt": prompt, "stream": False, "raw": False}
+    data = {
+        "model": "llama2-uncensored",
+        "prompt": prompt,
+        "stream": False,
+        "raw": False,
+    }
 
-    # Send POST request
     response = requests.post(
         api_url, data=json.dumps(data), headers={"Content-Type": "application/json"}
     )
-
-    # Parse JSON response
     json_response = response.json()
 
-    # Print the "response" field
     if "response" in json_response:
         print(json_response["response"])
-
     else:
         print("Error: Response field not found in the JSON response.")
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
     print("Extracting English Subtitle...")
     download_subtitle(url)
-    
+
     print("Summarizing Subtitle...")
-    summarize(get_prompt())
+    summarize(generate_prompt())
